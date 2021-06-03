838b7c874579e1ab654307a34d19dad5bf46e4f4
vibha <vibha.ch@gmail.com>
2021-06-02 20:31:31 -0400

    Add project files.

---
+++ GoNice/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using GoNice.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoNice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WhereToStay()
+        {
+            return View();
+        }
+        public IActionResult WhatToEat()
+        {
+            return View();
+        }
+
+        public IActionResult WhatToDo()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
